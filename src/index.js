ba306e6e2fe4943d95fc0ec6905eaf4a5552e32b
Rodrigodcs <rodrigodcsee@gmail.com>
2022-05-20 12:11:39 -0300

    Preparando revisão de código

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import ReactDOM from "react-dom";
-
-import "./Assets/reset.css";
-import "./Assets/style.css";
-
-import App from "./Components/App/App";
-
-
-ReactDOM.render(<App />, document.querySelector(".root"));
